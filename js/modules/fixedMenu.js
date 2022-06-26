f8f93122581563d6e26deae4a35964bb3aa41a62
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-06-25 21:23:27 -0300

    alteração da imagem menu mobile ao scroll

--- js/modules/fixedMenu.js
+++ js/modules/fixedMenu.js
@@ -1,10 +1,14 @@
+const btnMobileImage = document.querySelector('.btn-mobile img');
+
 export default function initFixedMenu() {
   const header = document.getElementById('js-header');
   function fixedMenu() {
     if (window.pageYOffset > 80) {
       header.classList.add('fixed-menu');
+      btnMobileImage.setAttribute('src', './img/menu-mobile-color.svg');
     } else {
       header.classList.remove('fixed-menu');
+      btnMobileImage.setAttribute('src', './img/menu-mobile.svg');
     }
   }
 
